1f7ff1782be574172e60a44bc101a7148500bd0c
Kamsiyochukwu-Orji <kamsiyochukwuorji@yahoo.com>
2026-05-06 09:31:26 +0100

    converted the React root component to vue root(top-level) component

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import AppErrorBoundary from "./shared/AppErrorBoundary.vue";
+</script>
+
+<template>
+  <AppErrorBoundary>
+    <RouterView />
+  </AppErrorBoundary>
+</template>
